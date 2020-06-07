017ac2b47713934430c4f56d00250fc19b73d25d
Yixi <lyh1112@gmail.com>
2020-06-07 22:00:21 +0800

    add a ss node ping test api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('@koa/router')
 const axios = require('axios')
 const {httping, ping} = require('./ping')
+const _ = require('lodash')
 
 const http = new axios.create({
   baseURL: 'http://192.168.50.1'
@@ -29,8 +30,12 @@ router
   .get('/ss-ping', async (ctx) => {
     const serverConfig = await http.get('/_api/ssconf_basic_server_');
     const servers = serverConfig.data.result[0]
-    // const result = await ping('47.99.141.172')
-    console.log(servers)
+    const serverlen = Object.keys(servers).length
+    const results = await Promise.all(
+      _.range(1, serverlen + 1)
+        .map(key => ping(servers[`ssconf_basic_server_${key}`]))
+    )
+    ctx.body = _.zipObject(_.range(1, serverlen + 1), results);
   })
 
 app.use(router.routes()).use(router.allowedMethods());
